b23f30690c9e4544d35978034be1c0c774d4fdce
ChristyMim <christy.mims@usm.edu>
2019-10-29 18:12:08 -0500

    Initial commit

---
+++ Mims_Christy Numbers/frmNumbers.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNumbers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.mtbQuantity = New System.Windows.Forms.MaskedTextBox()
+        Me.btnEnterNumbers = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(204, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "How many numbers do you wish to enter?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Must be less than 10)"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'mtbQuantity
+        '
+        Me.mtbQuantity.Location = New System.Drawing.Point(353, 26)
+        Me.mtbQuantity.Mask = "0"
+        Me.mtbQuantity.Name = "mtbQuantity"
+        Me.mtbQuantity.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.mtbQuantity.Size = New System.Drawing.Size(62, 20)
+        Me.mtbQuantity.TabIndex = 1
+        '
+        'btnEnterNumbers
+        '
+        Me.btnEnterNumbers.Location = New System.Drawing.Point(197, 71)
+        Me.btnEnterNumbers.Name = "btnEnterNumbers"
+        Me.btnEnterNumbers.Size = New System.Drawing.Size(111, 55)
+        Me.btnEnterNumbers.TabIndex = 2
+        Me.btnEnterNumbers.Text = "Enter" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Numbers"
+        Me.btnEnterNumbers.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(12, 152)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(490, 173)
+        Me.lstOutput.TabIndex = 3
+        '
+        'frmNumbers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(514, 337)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.btnEnterNumbers)
+        Me.Controls.Add(Me.mtbQuantity)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmNumbers"
+        Me.Text = "Numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents mtbQuantity As MaskedTextBox
+    Friend WithEvents btnEnterNumbers As Button
+    Friend WithEvents lstOutput As ListBox
+End Class
---
+++ Mims_Christy Numbers/frmNumbers.vb
@@ -0,0 +1,77 @@
+﻿Public Class frmNumbers
+    'Program: Numbers
+    'Programmer: Christy Mims
+    'Date: 3/29/17
+    'Description:  This program allows the user to input data and calculate the information.  The program does this through the use
+    'of Subrouteans, Functions, Do while and until loops, masked text boxes, list boxes, message boxes, and inputboxes.
+    Private Sub btnEnterNumbers_Click(sender As Object, e As EventArgs) Handles btnEnterNumbers.Click
+        'The event procedure where all the data is sent to.
+        Dim sum As Double = 0, average As Double = 0, min As Double = 0, max As Double = 0
+
+        If mtbQuantity.Text = "" Then
+            MessageBox.Show("Please specify the quantity of numbers that you wish to enter.", "Error")
+            mtbQuantity.Focus()
+        Else
+            lstOutput.Items.Clear()
+            Call GetNumbersAndStats(sum, average, min, max)
+            Call DisplayOutput(sum, average, min, max)
+        End If
+    End Sub
+    Sub GetNumbersAndStats(ByRef sum As Double, ByRef average As Double, ByRef min As Double, ByRef max As Double)
+        'Calculates and sends the data to the event procedure.
+        Dim quantity As Integer = CInt(mtbQuantity.Text)
+        Dim num, count As Double
+
+        count = 1
+        Do While count <= quantity
+            num = get_a_number(count)
+            If count = 1 Then
+                max = num
+                min = num
+            Else
+                If num > max Then
+                    max = num
+                End If
+                If num < min Then
+                    min = num
+                End If
+            End If
+            sum = sum + num
+            count = count + 1
+        Loop
+
+        average = sum / quantity
+
+    End Sub
+
+    Function get_a_number(count As Integer) As Double
+        'This function gathers the data and passes to sub GetNumbersAndStats.
+        Dim num As String
+
+        Do
+            num = InputBox("Enter number " & count.ToString, "Input")
+        Loop Until is_nonnegative_number(num)
+
+        Return CDbl(num)
+
+    End Function
+
+    Function is_nonnegative_number(num As String) As Boolean
+        'Function determines if number is nonnegative.
+        Dim valid As Boolean = False
+        If IsNumeric(num) Then
+            If CDbl(num) >= 0 Then
+                valid = True
+            End If
+        End If
+        Return valid
+    End Function
+
+    Sub DisplayOutput(ByVal sum As Double, ByVal average As Double, ByVal min As Double, ByVal max As Double)
+        'Copies and sends the information to event procedure to display in the list box.
+        lstOutput.Items.Add("Sum: " & sum)
+        lstOutput.Items.Add("Average: " & Math.Round(average, 0))
+        lstOutput.Items.Add("Minimum: " & min)
+        lstOutput.Items.Add("Maximum: " & max)
+    End Sub
+End Class
